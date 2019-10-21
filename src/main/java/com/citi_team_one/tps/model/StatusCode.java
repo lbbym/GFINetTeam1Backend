e5e314e0d62d6ca5f00bc1c6edf12de130042abd
lbbym <liubbpro@gmail.com>
2019-10-21 18:12:45 +0800

    match

--- src/main/java/com/citi_team_one/tps/model/StatusCode.java
+++ src/main/java/com/citi_team_one/tps/model/StatusCode.java
@@ -1,7 +1,7 @@
 package com.citi_team_one.tps.model;
 
 public enum StatusCode {
-    REQUESTED(), PENDING(), TPS_PROCESSED(), ACCEPTED(), REJECTED();
+    REQUESTED(), PENDING(), TPS_PROCESSED(), ACCEPTED(), REJECTED(), PRICE_UNMATCHED();
 
     private RejectCode rejectCode;
 
