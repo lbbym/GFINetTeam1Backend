1bdbc2b9d205b3a9ff4a866cf11c7e0eaf3fe5f6
anuding <anuding@126.com>
2019-10-21 09:34:38 +0800

    Add Service

--- src/main/java/com/citi_team_one/tps/mapper/UserMapper.java
+++ src/main/java/com/citi_team_one/tps/mapper/UserMapper.java
@@ -5,15 +5,7 @@ import com.citi_team_one.tps.model.User;
 import java.util.List;
 
 public interface UserMapper {
-
-    List<User> getAll();
-
-    User getOne(String id);
-//
-//    void insert(User user);
-//
-//    void update(User user);
-//
-//    void delete(String id);
-
+    List<User> doFindAll();
+    User doFindByName(String name);
+    User doFindById(Integer id);
 }
