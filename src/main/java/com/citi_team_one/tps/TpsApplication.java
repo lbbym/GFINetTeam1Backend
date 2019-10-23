cd7ba47b45692f45b3489a9168234c97adc4dec1
shike <471791040@qq.com>
2019-10-23 13:34:59 +0800

    change model

--- src/main/java/com/citi_team_one/tps/TpsApplication.java
+++ src/main/java/com/citi_team_one/tps/TpsApplication.java
@@ -3,9 +3,9 @@ package com.citi_team_one.tps;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ActiveMQAutoConfiguration.class)
 @MapperScan("com.citi_team_one.tps.mapper")
 public class TpsApplication {
     public static void main(String[] args) {
